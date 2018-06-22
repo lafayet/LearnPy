8e7221fd73f87bde7967543966ad3152a5774be0
lafayet <34798302+lafayet@users.noreply.github.com>
2018-06-22 08:21:49 +0300

    Code restructurised, help message added

--- pyGame/Camels/camels_game.py
+++ pyGame/Camels/camels_game.py
@@ -2,28 +2,53 @@ import pygame, sys
 
 from pygame.locals import *
 pygame.init()
-fpsClock = pygame.time.Clock()
+pygame.display.set_caption("Camel Game")
 
-#useful game dimensions
-TILESIZE = 40
-MAPWIDTH = 9
-MAPHEIGHT = 1
+TILESIZE = 40 #square tiles, side size in px
+MAPTILEWIDTH = 9
+MAPTILEHEIGHT = 1
+MAPWIDTHPX = MAPTILEWIDTH*TILESIZE
+MAPHEIGHTPX = MAPTILEHEIGHT*TILESIZE
+BARFORTEXTSPX = 30
 
 recources_path = 'recources/'
 
-DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE+30))
+#create main screen
+DISPLAYSURF = pygame.display.set_mode((MAPWIDTHPX,MAPHEIGHTPX+BARFORTEXTSPX))
+#init font
 INVFONT = pygame.font.Font(recources_path + 'Arial.ttf', 20)
+HLPFONT = pygame.font.Font(recources_path + 'Arial.ttf', 12)
+#init fps count
+fpsClock = pygame.time.Clock()
 
-#constants representing colors
-BLACK = (0,   0,   0  )
-BROWN = (153, 76,  0  )
-GREEN = (0,   255, 0  )
-BLUE  = (0,   0,   255)
-GREY  = (120, 120, 120)
+#colors
+C_BLACK = (0,   0,   0  )
 С_RED   = (255, 0,   0  )
-WHITE = (255, 255, 255)
+C_WHITE = (255, 255, 255)
 
-#constants representing the different recources
+#game texts
+helpMsgStr1 =  "Rounds is camels. Red camels can move only right, blue - left."
+helpMsgStr2 = "Camel can move on free space near or jump over other colored"
+helpMsgStr3 = "camel if there free space behind it. To win, place blue camels"
+helpMsgStr4 = "to left side and red to right side.Restart will reset game."
+helpMsgStr5 = "Click to start."
+
+restartTxtHovered = INVFONT.render("Restart", True, С_RED, C_WHITE)
+restartTxt = INVFONT.render("Restart", True, C_BLACK, C_WHITE)
+winMsg = INVFONT.render("You won! Congratulations!", True, C_BLACK, C_WHITE)
+
+#pygame can't blit multiple line string. So there is five strings for one help message
+helpMsg1 = HLPFONT.render(helpMsgStr1, True, C_BLACK, C_WHITE)
+helpMsg2 = HLPFONT.render(helpMsgStr2, True, C_BLACK, C_WHITE)
+helpMsg3 = HLPFONT.render(helpMsgStr3, True, C_BLACK, C_WHITE)
+helpMsg4 = HLPFONT.render(helpMsgStr4, True, C_BLACK, C_WHITE)
+helpMsg5 = HLPFONT.render(helpMsgStr5, True, C_BLACK, C_WHITE)
+
+#game text position on screen
+textX = MAPWIDTHPX - 10 - restartTxtHovered.get_width()
+textY = MAPHEIGHTPX + 5
+
+#camels types
 NOTHING = 0
 RED = 1
 BLUE = 2
@@ -36,11 +61,9 @@ textures =   {
                 SELECTED: pygame.image.load(recources_path + 'selected.png').convert_alpha()
             }
 
-pygame.display.set_caption("Camel Game")
-
 class Camel:
-    x = 0
-    skin = NOTHING
+    x = 0 #x position of camel
+    skin = NOTHING #camel type
     def __init__ (self, x, skin, name):
         self.x = x
         self.skin = skin
@@ -50,6 +73,7 @@ class Camel:
         result = self.x < other.x
         return result
 
+    # method to move camels if possible
     def move (self, x, camel_list):
         red = self.skin == RED
         blue = self.skin == BLUE
@@ -96,19 +120,6 @@ class Camel:
         #else:
             #print ("Oops! You cannot move it here.")
 
-camelSelected1 = False
-camelSelected2 = False
-restartHover = False
-x1 = -1
-x2 = -1
-tileClicked = [-1,-1]
-mouse_pos = [0.0, 0.0]
-textObj1 = INVFONT.render("Restart", True, С_RED, WHITE)
-textObj2 = INVFONT.render("Restart", True, BLACK, WHITE)
-winMsg = INVFONT.render("You won! Congratulations!", True, BLACK, WHITE)
-textX = MAPWIDTH*TILESIZE - 10 - textObj1.get_width()
-textY = MAPHEIGHT*TILESIZE+5
-click = False
 camels = [Camel(0,RED, "RedONE"),
           Camel(1,RED, "RedTWO"),
           Camel(2,RED, "RedTHREE"),
@@ -118,26 +129,48 @@ camels = [Camel(0,RED, "RedONE"),
           Camel(6,BLUE, "BlueTWO"),
           Camel(7,BLUE, "BlueTHREE"),
           Camel(8,BLUE, "BlueFOUR")]
-skins = ("Nothing", "Red", "Blue")        
+skins = ("Nothing", "Red", "Blue")  
+
+# bool flags to control game state
+camelSelected1 = False  # camel which will be moved selected flag
+camelSelected2 = False  # camel at which place user want to move first camel (free space is also camels)
+restartHover = False    # restart text hovered flag
+click = False           # mouse click flag
+showHelp = True
+
+x1 = -1                 # x position of first camel
+x2 = -1                 # x position of second
+tileClicked = [-1,-1]
+mouse_pos = [0.0, 0.0]
+
+# main game loop      
 while True:
-    DISPLAYSURF.fill(WHITE)
+    #clear screen with white color
+    DISPLAYSURF.fill(C_WHITE)
     mouse_pos = pygame.mouse.get_pos()
     click = False
+
+    #process events
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()           
         elif event.type == MOUSEBUTTONUP:
-            tileClicked[0] = mouse_pos[0]//TILESIZE
-            tileClicked[1] = mouse_pos[1]//TILESIZE
-            click = True
+            if not showHelp:
+                tileClicked[0] = mouse_pos[0]//TILESIZE
+                tileClicked[1] = mouse_pos[1]//TILESIZE
+                click = True
+            else:
+                showHelp = False
             #print (tileClicked)
 
-    if (mouse_pos[0] > textX) and (mouse_pos[0] < MAPWIDTH*TILESIZE - 10) and (mouse_pos[1] > textY) and (mouse_pos[1] < MAPHEIGHT*TILESIZE+30) :
+    #check that restart is hovered
+    if (mouse_pos[0] > textX) and (mouse_pos[0] < MAPWIDTHPX - 10) and (mouse_pos[1] > textY) and (mouse_pos[1] < MAPHEIGHTPX+BARFORTEXTSPX) :
         restartHover = True
     else:
         restartHover = False
 
+    #reset game
     if restartHover and click:
         camels = [Camel(0,RED, "RedONE"),
           Camel(1,RED, "RedTWO"),
@@ -149,31 +182,54 @@ while True:
           Camel(7,BLUE, "BlueTHREE"),
           Camel(8,BLUE, "BlueFOUR")]
         click = False
+
+    #define first selected camel
     if not camelSelected1 and (tileClicked[0] >= 0 and tileClicked[1] == 0):
         camelSelected1 = True
         x1 = tileClicked[0]
         tileClicked[0] = -1
+
+    #define second selected camel
     if not camelSelected2 and camelSelected1 and (tileClicked[0] >= 0 and tileClicked[1] == 0):
         camelSelected2 = True
         x2 = tileClicked[0]
         tileClicked[0] = -1
+
+    #move camels
     if camelSelected1 and camelSelected2:
         camels[x1].move(x2, camels)
         #for c in camels:
             #print ("Camel number = " + str(c.x) + " it's:" + skins[c.skin] + " it's name: " + c.name)    
         camelSelected1 = False
         camelSelected2 = False
+
+    #draw camels
     for c in camels:         
         DISPLAYSURF.blit(textures[c.skin],[c.x*TILESIZE,0])
+
+    #draw selection of camel
     if camelSelected1:
         DISPLAYSURF.blit(textures[SELECTED],[x1*TILESIZE,0])
+
     if restartHover:
-        textObj = textObj1
+        textObj = restartTxtHovered
     else:
-        textObj = textObj2
-    
+        textObj = restartTxt
+
+    #draw restart
     DISPLAYSURF.blit(textObj,(textX, textY))
+
+    #check win condition and show win msg if so
     if camels[0].skin == BLUE and camels[1].skin == BLUE and camels[2].skin == BLUE and camels[3].skin == BLUE and camels[4].skin == NOTHING and camels[5].skin == RED and camels[6].skin == RED and camels[7].skin == RED and camels[8].skin == RED:
         DISPLAYSURF.blit(winMsg,(10, textY))    
+
+    if showHelp:
+        DISPLAYSURF.fill(C_WHITE)
+        DISPLAYSURF.blit(helpMsg1,(5, 2))
+        DISPLAYSURF.blit(helpMsg2,(5, 15))
+        DISPLAYSURF.blit(helpMsg3,(5, 28))
+        DISPLAYSURF.blit(helpMsg4,(5, 41))
+        DISPLAYSURF.blit(helpMsg5,(5, 54))
+    #update screen
     pygame.display.update()
     fpsClock.tick(24)
